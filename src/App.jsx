d4294e41ca2258983329389cb164cbb09d79e9fc
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-04-15 17:50:26 +0530

    thunk implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement } from "./features/Counter/counterSlice";
+import {
+  increment,
+  decrement,
+  fetchCountValue,
+} from "./features/Counter/counterSlice";
 
 function App() {
   const dispatch = useDispatch();
-  const { value } = useSelector((s) => s.counter);
+  const { value, loading } = useSelector((s) => s.counter);
   return (
     <>
       <div>
         <h2>React + Redux</h2>
-        <h3>{value}</h3>
+        <h3> count : {value}</h3>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(fetchCountValue())}>
+          {loading ? "...loading" : "added +5"}
+        </button>
       </div>
     </>
   );
--- src/features/Counter/counterSlice.js
+++ src/features/Counter/counterSlice.js
@@ -1,8 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchCountValue = createAsyncThunk(
+  "counter/fetchValue",
+  async () => {
+    await new Promise((r) => setTimeout(r, 3000));
+    return 5;
+  },
+);
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState: { value: 0 },
+  initialState: { value: 0, loading: false },
   reducers: {
     increment: (state) => {
       state.value += 1;
@@ -10,6 +18,20 @@ const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
+  },
+
+  // extraReducer is used to handle async operation
+  extraReducers: (builder) => {
+    builder.addCase(fetchCountValue.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchCountValue.fulfilled, (state, action) => {
+      state.loading = false;
+      state.value += action.payload;
+    });
+    builder.addCase(fetchCountValue.rejected, (state) => {
+      state.loading = false;
+    });
   },
 });
 export const { increment, decrement } = counterSlice.actions;
